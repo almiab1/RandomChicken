c2f6455b21b194c5693423d5ed627f550fb06147
almiab1 <almiab1@epsg.upv.es>
2020-03-28 00:55:51 +0100

    jail game + menu scene + loading system

--- Assets/GameManager.cs
+++ Assets/GameManager.cs
@@ -1,11 +1,48 @@
 ﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
 
 public class GameManager : MonoBehaviour
 {
     public int numberOfBounces = 0;
+    public GameObject img1000Bounces;
 
+    private void Update()
+    {
+        if (numberOfBounces == 1000)
+        {
+            activateImage(img1000Bounces);
+            StartCoroutine(hideImage(img1000Bounces));
+
+        }
+
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            if (Cursor.visible == false)
+            {
+                Cursor.visible = true;
+            }
+            else
+            {
+                Cursor.visible = false;
+            }
+            Debug.Log(Cursor.visible);
+        }
+    }
     private void Start()
     {
         Cursor.visible = false;
+    }
+
+    private void activateImage(GameObject imageToActivate)
+    {
+        imageToActivate.SetActive(true);
+    }
+
+    private IEnumerator hideImage(GameObject imageTOHide)
+    {
+        yield return new WaitForSeconds(4f);
+        imageTOHide.SetActive(false);
+
     }
 }
---
+++ Assets/Scripts/detectObject.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class detectObject : MonoBehaviour
+{
+    public Transform referencePoint;
+    public GameObject luz;
+    private Transform player;
+    public GameObject luzEmergendia;
+
+    protected bool seguimiento;
+
+    private void Update()
+    {
+        if(seguimiento == true)
+        {
+            var targetRotation = Quaternion.LookRotation(player.position - luz.transform.position);
+
+            // Smoothly rotate towards the target point.
+            luz.transform.rotation = Quaternion.Slerp(luz.transform.rotation, targetRotation, 3 * Time.deltaTime);
+
+            //luz.transform.LookAt(player);
+        }
+        else
+        {
+            //luz.transform.LookAt(referencePoint);
+
+            var targetRotation = Quaternion.LookRotation(referencePoint.position - luz.transform.position);
+
+            // Smoothly rotate towards the target point.
+            luz.transform.rotation = Quaternion.Slerp(luz.transform.rotation, targetRotation, 1 * Time.deltaTime);
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        Debug.Log("HA ENTRADO EN LA ZONA");
+        if (other.gameObject.tag == "Player")
+        {
+            seguimiento = true;
+            player = other.transform;
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.gameObject.tag == "Player")
+        {
+            seguimiento = false;
+        }
+        Debug.Log("HA SALIDO DE LA ZONA");
+    }
+}
